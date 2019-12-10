0a22daecbb226e6c6cf3ceb2a3f695f1fa601b70
Griffith <2ndgriff@gmail.com>
2019-12-10 19:44:02 +0100

    shouldve been committing all this time but created a login component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
+import './App.css';
 
 import Signup from './pages/Signup';
 import Login from './pages/Login';
 import Private from './pages/Private';
-import Navbar from './components/Navbar';
 
 import AnonRoute from './components/AnonRoute';
 import PrivateRoute from './components/PrivateRoute';
@@ -13,9 +13,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Navbar />
-        <h1>Basic React Authentication</h1>
-
         <Switch>
           <AnonRoute exact path="/signup" component={Signup} />
           <AnonRoute exact path="/login" component={Login} />
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { withAuth } from '../lib/AuthProvider';
+import { Link } from 'react-router-dom';
 
 class Login extends Component {
   state = { username: '', password: '' };
@@ -19,27 +20,37 @@ class Login extends Component {
     const { username, password } = this.state;
 
     return (
-      <div>
-        <h1>Login</h1>
-        <form onSubmit={this.handleFormSubmit}>
-          <label>Username:</label>
-          <input
-            type="text"
-            name="username"
-            value={username}
-            onChange={this.handleChange}
-          />
+      <div className = 'logincontainer'>
+        <h1>Project Canary</h1>
+        <div className = 'loginpage'>
+          <div className = 'loginorsignup'>
+            <h1>Login</h1>
+            <p>or...</p>
+            <Link to="/signup">
+                {' '}
+                <button className = 'buttonn' >Signup</button>{' '}
+            </Link>
+          </div>
+          <form onSubmit={this.handleFormSubmit}>
+            <label>Username:</label>
+            <input
+              type="text"
+              name="username"
+              value={username}
+              onChange={this.handleChange}
+            />
 
-          <label>Password:</label>
-          <input
-            type="password"
-            name="password"
-            value={password}
-            onChange={this.handleChange}
-          />
+            <label>Password:</label>
+            <input
+              type="password"
+              name="password"
+              value={password}
+              onChange={this.handleChange}
+            />
 
-          <input type="submit" value="Login" />
-        </form>
+           <button id = 'loginbutton'>Submit</button>
+          </form>
+        </div>
       </div>
     );
   }
